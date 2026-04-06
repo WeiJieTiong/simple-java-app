8de2886063cf1edcd849ec5d2c049bdf3ac43502
Tiong Wei Jie <weijietiong@icloud.com>
2026-04-06 15:16:28 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("user 11 blah blah blah");
+        System.out.println("modified again n again");
     }
 
     public String getMessage() {
