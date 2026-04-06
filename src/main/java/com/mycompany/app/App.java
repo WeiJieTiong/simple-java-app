696414a1f46046f32b7da090483a761724ccba95
Tiong Wei Jie <weijietiong@icloud.com>
2026-04-06 15:22:31 +0800

    My second change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("modified again n again");
+        System.out.println("change agn");
     }
 
     public String getMessage() {
